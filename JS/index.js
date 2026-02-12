23065604fa2f1845230d1f796613529182a3652b
matthewcairns15 <matthewcairns15@outlook.com>
2026-02-12 13:00:45 +0000

    index fix

--- JS/index.js
+++ JS/index.js
@@ -57,7 +57,7 @@ let rsvp;
 // ===== Render Invitees Dynamically =====
 function renderInvitees() {
   const container = document.getElementById("invitee-names");
-  //container.innerHTML = ""; // clear container
+  container.innerHTML = ""; // clear container
 
   rsvp.invitees.forEach((invitee, index) => {
     const div = document.createElement("div");
